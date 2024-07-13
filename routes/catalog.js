1d7408c27acc621a3d0a645f5be0f01fc232b242
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-13 17:47:07 +0100

    feat: Set route for the nome page

---
+++ routes/catalog.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// Require controller modules
+const author_controller = require("../controllers/authorController");
+const comic_controller = require("../controllers/comicController");
+
+// Get home page
+router.get("/", comic_controller.index);
+
+// Author routes
