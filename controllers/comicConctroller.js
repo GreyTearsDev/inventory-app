b53317c98a08a9e44ff7ecb4fc8f17fa861dfd19
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-13 18:38:58 +0100

    refactor: Update comic controller
    - Correct model imports: Author, Publisher, Genre, and Volume
    - Replace individual document queries with count queries
    - Use Promise.all to fetch counts of comics, volumes, authors, publishers, and genres concurrently
    - Update render method to pass counts instead of lists to the index view

--- controllers/comicConctroller.js
+++ controllers/comicConctroller.js
@@ -1,25 +1,29 @@
 const Comic = require("../models/comic");
-const Authors = require("../models/authors");
-const Publishers = require("../models/publishers");
-const Genres = require("../models/genres");
+const Author = require("../models/author");
+const Publisher = require("../models/publisher");
+const Genre = require("../models/genre");
+const Volume = require("../models/volume");
 
 const asyncHandler = require("async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
   // Get details about Comics, Authors, Publishers, and Genres
 
-  const [allComics, allAuthors, allPublishers, allGenres] = await Promise.all([
-    Comic.find({}, "title author").sort({ title: 1 }).exec(),
-    Author.find().sort({ first_name: 1 }).exec(),
-    Publisher.find({}, "name").sort({ name: 1 }).exec(),
-    Genre.find().sort({ name: 1 }).exec(),
-  ]);
+  const [comicCount, volumeCount, authorCount, publisherCount, genreCount] =
+    await Promise.all([
+      Comic.countDocuments({}).exec(),
+      Volume.countDocuments({}).exec(),
+      Author.countDocuments({}).exec(),
+      Publisher.countDocuments({}).exec(),
+      Genre.countDocuments({}).exec(),
+    ]);
 
   res.render("index", {
     title: "ComiKing - Home",
-    comic_list: allComics,
-    author_list: allAuthors,
-    publisher_list: allPublishers,
-    genre_list: allGenres,
+    comic_count: comicCount,
+    volume_count: volumeCount,
+    author_count: authorCount,
+    publisher_count: publisherCount,
+    genre_count: genreCount,
   });
 });
