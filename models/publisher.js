24a226c7ff456bbe0b46114fcdefd1ff83e05d31
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-14 19:36:01 +0100

    refactor: Update Publisher schema in publisher.js
    - Removed unnecessary parentheses from Schema declaration.
    - Added 'comics' array to link Publisher to Comic model via Schema.Types.ObjectId.
    - Defined maxLength and required constraints for 'name' field.
    - Added 'headquarters' field with maxLength constraint.
    - Implemented virtual 'info' property getter for additional publisher information.

--- models/publisher.js
+++ models/publisher.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
 
 const PublisherSchema = new Schema({
   name: { type: String, required: true, maxLength: 40 },
   headquarters: { type: String, maxLength: 40 },
+  comics: [{ type: Schema.Types.ObjectId, ref: "Comic" }],
 });
 
 PublisherSchema.virtual("info").get(function () {
