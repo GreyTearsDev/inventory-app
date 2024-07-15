f605f02c12f642293ab24e06cd8cfb7f6c904000
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-15 15:54:45 +0100

    feat: Integrate MongoDB, dotenv, express-ejs-layouts, and catalog route
    - Added dotenv configuration for environment variables.
    - Added express-ejs-layouts middleware for layout support in views.
    - Integrated MongoDB connection using mongoose and environment variable for MongoDB URI.
    - Replaced usersRouter with catalogRouter for catalog route handling.

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const createError = require("http-errors");
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const createError = require("http-errors");
+const expressLayouts = require("express-ejs-layouts");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 const app = express();
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(expressLayouts);
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -20,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
