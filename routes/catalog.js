f1365f8f2ec452f24d186d71f5f92d2a49027d4e
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-16 17:07:43 +0100

    feat: Add publisher routes to catalog
    - Imported publisher_controller for handling publisher-related routes
    - Added route for displaying the list of all publishers
    - Maintained existing routes and organized them with comments for clarity

--- routes/catalog.js
+++ routes/catalog.js
@@ -4,6 +4,10 @@ const router = express.Router();
 // Require controller modules
 const author_controller = require("../controllers/authorController");
 const comic_controller = require("../controllers/comicController");
+const genre_controller = require("../controllers/genreController");
+const publisher_controller = require("../controllers/publisherController");
+
+// == COMIC controller == //
 
 // Get home page
 router.get("/", comic_controller.index);
@@ -11,5 +15,13 @@ router.get("/", comic_controller.index);
 // Display list of all comics
 router.get("/comics", comic_controller.comic_list);
 
-// Author routes
+// == AUTHOR controller == //
+router.get("/authors", author_controller.author_list);
+
+// == GENRE controller == //
+router.get("/genres", genre_controller.genre_list);
+
+// == PUBLISHER controller == //
+router.get("/publishers", publisher_controller.publisher_list);
+
 module.exports = router;
