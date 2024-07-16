573996ef99b9ed7e99af097d0ed5081128a7e27c
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-16 15:31:13 +0100

    refactor: Simplify author list retrieval and rendering in controller
    - Removed unnecessary population of Comic in author list query
    - Renamed 'authors' to 'allAuthors' for clarity
    - Adjusted the title in the render method from 'List of Authors' to 'Authors'

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -3,10 +3,7 @@ const asyncHandler = require("express-async-handler");
 
 // Display list of all authors
 exports.author_list = asyncHandler(async (req, res, next) => {
-  const authors = await Author.find()
-    .sort({ first_name: 1 })
-    .populate("Comic")
-    .exec();
+  const allAuthors = await Author.find().sort({ first_name: 1 }).exec();
 
-  res.render("author_list", { title: "List of Authors", author_list: authors });
+  res.render("author_list", { title: "Authors", author_list: allAuthors });
 });
