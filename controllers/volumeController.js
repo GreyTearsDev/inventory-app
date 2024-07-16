3ce1808a45b8f418d8a97632f3ab15e0b53e83aa
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-16 23:26:24 +0100

    feat: Implement volume_detail controller function
    Added a controller function 'volume_detail' to handle requests for retrieving volume details by ID from the database.

--- controllers/volumeController.js
+++ controllers/volumeController.js
@@ -1,1 +1,16 @@
+const Volume = require("../models/volume");
+const asyncHandler = require("express-async-handler");
 
+
+exports.volume_detail = asyncHandler(async(req, res, next) => {
+  console.log(req.params.id)
+  const volume = await Volume.findById(req.params.id).exec();
+
+  if (!volume) {
+    const err = new Error("Volume not found");
+    err.status = 404;
+    return next(err);
+  }
+console.log("oooi")
+  res.render("volume_detail", {title: "Volume details", volume: volume})
+})
