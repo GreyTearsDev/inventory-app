7188613a5970256ade0383e937589b0b15580d8b
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-16 20:57:42 +0100

    fix: Fix routing order  to ensure routes with specific ids are accessed correctly.
    - Updated routes for comic, author, genre, and publisher detail views:
    - Moved '/comic/:id', '/author/:id', '/genre/:id' and '/publisher/:id' routes before their respective list routes.
    - Ensured proper ordering to avoid route conflicts and improve route resolution.

--- routes/catalog.js
+++ routes/catalog.js
@@ -13,20 +13,20 @@ const publisher_controller = require("../controllers/publisherController");
 router.get("/", comic_controller.index);
 
 // Display list of all comics
+router.get("/comic/:id", comic_controller.comic_detail);
 router.get("/comics", comic_controller.comic_list);
-router.get("/comics/:id", comic_controller.comic_detail);
 
 // == AUTHOR controller == //
+router.get("/author/:id", author_controller.author_detail);
 router.get("/authors", author_controller.author_list);
-router.get("/authors/:id", author_controller.author_detail);
 
 // == GENRE controller == //
+router.get("/genre/:id", genre_controller.genre_detail);
 router.get("/genres", genre_controller.genre_list);
-router.get("/genres/:id", genre_controller.genre_detail);
 
 // == PUBLISHER controller == //
+router.get("/publisher/:id", publisher_controller.publisher_detail);
 router.get("/publishers", publisher_controller.publisher_list);
-router.get("/publishers/:id", publisher_controller.publisher_detail);
 
 module.exports = router;
 
