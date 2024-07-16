4904a9391f9e55bb0af2958d029b5c9d0024c605
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-16 21:42:57 +0100

    feat: Implement author detail retrieval and rendering

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,5 @@
 const Author = require("../models/author");
+const Comic = require("../models/comic")
 const asyncHandler = require("express-async-handler");
 
 // Display list of all authors
@@ -7,3 +8,19 @@ exports.author_list = asyncHandler(async (req, res, next) => {
 
   res.render("author_list", { title: "Authors", author_list: allAuthors });
 });
+
+
+exports.author_detail = asyncHandler(async(req, res, next) => {
+  const [author, comicsByAuthor] = await Promise.all([
+    Author.findById(req.params.id).sort({first_name: 1}).exec(),
+    Comic.find({author: req.params.id}, "title").sort({title: 1}).exec()
+  ]);
+
+  if (!author) {
+    const err = new Erro("Author not found");
+    err.status = 404;
+    return next(err)
+  }
+  
+  res.render("author_detail", {title: "Author Profile", author: author, comic_list: comicsByAuthor})
+})
