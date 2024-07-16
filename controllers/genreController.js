f6216bb1a7ba022e8d697fe2443e44f15adf5797
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-17 00:04:17 +0100

    feat: Implement genre detail retrieval and rendering

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,8 +1,31 @@
 const Genre = require("../models/genre");
+const Comic = require("../models/comic");
+const {ObjectId} = require("mongoose").Types
 const asyncHandler = require("express-async-handler");
 
+// List all currently available genres
 exports.genre_list = asyncHandler(async (req, res, next) => {
   const allGenres = await Genre.find().sort({ name: 1 }).exec();
 
   res.render("genre_list", { title: "Genres", genre_list: allGenres });
 });
+
+exports.genre_detail = asyncHandler(async (req, res, next) => {
+  const genreId = req.params.id
+  const [genre, comicsOfGenre ]= await Promise.all([
+    Genre.findById(genreId).exec(),
+    Comic.find({ genres: genreId }, "title").exec()
+  ]) ;
+
+  if (!genre) {
+    const err = new Error("Genre not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("genre_detail", { 
+    title: "Genre Detail", 
+    genre: genre, 
+    comic_list: comicsOfGenre 
+  }) 
+})
