f2eddb7180c2a8e8a51056e772191a5ccb10ea73
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-20 12:17:43 +0100

    feat: Implement genre creation POST route with validation and error handling
    - Added 'express-validator' to validate and sanitize the input.
    - Implemented 'genre_create_post' controller function to handle POST requests for genre creation.
    - validated the 'name' field to ensure it is not empty and escapes special characters.
    - Added error handling to render the form again with validation errors if any are found.
    - Checked for existing genres before saving a new genre.
    - Redirected to the existing genre's URL if a duplicate is found or saved the new genre and redirected to its URL.

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,7 @@
 const Genre = require("../models/genre");
 const Comic = require("../models/comic");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // List all currently available genres
 exports.genre_list = asyncHandler(async (req, res, next) => {
@@ -37,4 +38,39 @@ exports.genre_create_get = (req, res, next) => {
   });
 };
 
-// exports.genre_create_post = [asyncHandler(async (req, res, next) => {})];
+exports.genre_create_post = [
+  body("name", "Name field must be filled")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const genre = new Genre({
+      name: req.body.name,
+    });
+
+    if (!errors.isEmpty) {
+      res.render("genre_form", {
+        title: "Create new genre",
+        genre: undefined,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // check if genre already exists
+      const genreExists = await Genre.findOne({ name: req.params.name })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+
+      if (genreExists) {
+        res.redirect(genreExists.url);
+      } else {
+        console.log("save it");
+        await genre.save();
+        res.redirect(genre.url);
+      }
+    }
+  }),
+];
