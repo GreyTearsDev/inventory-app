99947dc86f9b278fe0ea40a7addb5e631cfa20a0
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-21 15:36:12 +0100

    feat: Add validation and sanitization to incoming form data using 'express-validator'

--- controllers/volumeController.js
+++ controllers/volumeController.js
@@ -1,5 +1,6 @@
 const Volume = require("../models/volume");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.volume_detail = asyncHandler(async (req, res, next) => {
   console.log(req.params.id);
@@ -23,3 +24,36 @@ exports.volume_create_get = (req, res, next) => {
     errors: [],
   });
 };
+
+exports.volume_creat_post = [
+  body("volume_volume")
+    .isNumeric()
+    .withMessage("Volume number must be a numeric value")
+    .isInt({ min: 0, max: 1000000 })
+    .withMessage(
+      "Volume number must be between at least 0 and not greater than 1.000.000",
+    )
+    .escape(),
+
+  body("volume_title")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Title field must be filled")
+    .isLength({ max: 100 })
+    .withMessage("This title is too long")
+    .escape(),
+
+  bosy("volume_description")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Description field must be filled")
+    .isLength({ max: 200 })
+    .withMessage("This description is too long")
+    .escape(),
+
+  body("volume_release_date", "Invalid date")
+    .optional({ value: "falsy" })
+    .isISO8601()
+    .toDate()
+    .escape(),
+];
