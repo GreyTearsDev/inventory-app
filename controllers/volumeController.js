6d0883d39761027e9148a1f7837de4f043f0545f
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-21 15:20:18 +0100

    feat: Add volume creation GET handler
    - It renders volume creation form with default values.

--- controllers/volumeController.js
+++ controllers/volumeController.js
@@ -1,9 +1,8 @@
 const Volume = require("../models/volume");
 const asyncHandler = require("express-async-handler");
 
-
-exports.volume_detail = asyncHandler(async(req, res, next) => {
-  console.log(req.params.id)
+exports.volume_detail = asyncHandler(async (req, res, next) => {
+  console.log(req.params.id);
   const volume = await Volume.findById(req.params.id).exec();
 
   if (!volume) {
@@ -11,6 +10,16 @@ exports.volume_detail = asyncHandler(async(req, res, next) => {
     err.status = 404;
     return next(err);
   }
-console.log("oooi")
-  res.render("volume_detail", {title: "Volume details", volume: volume})
-})
+  res.render("volume_detail", { title: "Volume details", volume: volume });
+});
+
+exports.volume_create_get = (req, res, next) => {
+  res.render("volume_create", {
+    title: "Create new volume",
+    volume_number: undefined,
+    volume_title: undefined,
+    volume_description: undefined,
+    volume_release_date: undefined,
+    errors: [],
+  });
+};
