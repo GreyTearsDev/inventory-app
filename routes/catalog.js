9bd8e688ff919f6c4bf26ccd464fb45610369a04
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-22 22:12:06 +0100

    feat: Add post route for updating author

--- routes/catalog.js
+++ routes/catalog.js
@@ -32,6 +32,7 @@ router.get("/comics", comic_controller.comic_list);
 router.get("/author/create", author_controller.author_create_get);
 router.post("/author/create", author_controller.author_create_post);
 router.get("/author/:id/update", author_controller.author_update_get);
+router.post("/author/:id/update", author_controller.author_update_post);
 router.get("/author/:id", author_controller.author_detail);
 router.get("/authors", author_controller.author_list);
 
