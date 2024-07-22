845b60d315629e34ea5dfc103cc18fdb99ed3568
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-22 23:11:09 +0100

    feat: Add POST route for updating comic info

--- routes/catalog.js
+++ routes/catalog.js
@@ -26,6 +26,7 @@ router.post(
 router.get("/comic/create", comic_controller.comic_create_get);
 router.post("/comic/create", comic_controller.comic_create_post);
 router.get("/comic/:id/update", comic_controller.comic_update_get);
+router.post("/comic/:id/update", comic_controller.comic_update_post);
 router.get("/comic/:id", comic_controller.comic_detail);
 router.get("/comics", comic_controller.comic_list);
 
