752c3d67d03aeaaa713430901407b05fdc6e63d4
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-23 12:02:47 +0100

    feat: Add associated comic to volume details and error handling
    - Added retrieval of associated comic in 'volume_detail' controller.
    - Implemented error handling to return a 404 error if the volume is not associated with any comic.
    - Updated 'volume_detail' view to include the associated comic details.
    - Removed unnecessary console log statement.

--- controllers/volumeController.js
+++ controllers/volumeController.js
@@ -1,26 +1,26 @@
 const Volume = require("../models/volume");
+const Comic = require("../models/comic");
 const asyncHandler = require("express-async-handler");
-const { body, validationResult } = require("express-validator");
 
 exports.volume_detail = asyncHandler(async (req, res, next) => {
-  console.log(req.params.id);
   const volume = await Volume.findById(req.params.id).exec();
+  const associatedComic = await Comic.findOne({ volumes: volume._id }).exec();
 
   if (!volume) {
     const err = new Error("Volume not found");
     err.status = 404;
     return next(err);
   }
-  res.render("volume_detail", { title: "Volume details", volume: volume });
-});
 
-exports.volume_create_get = (req, res, next) => {
-  res.render("volume_create", {
-    title: "Create new volume",
-    volume_number: undefined,
-    volume_title: undefined,
-    volume_description: undefined,
-    volume_release_date: undefined,
-    errors: [],
+  if (!associatedComic) {
+    const err = new Error("This volume is not associated to any comic");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("volume_detail", {
+    title: "Volume details",
+    volume: volume,
+    comic: associatedComic,
   });
-};
+});
