ee092c30b47d98631085f2a1d30926a5409eb69c
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-23 20:08:42 +0100

    feat: Add volume update functionality
    - Added volume_update_get to render volume update form.
    - Implemented volume_update_post with validation and error handling.
    - Removed console logs from volume_delete_post.

--- controllers/volumeController.js
+++ controllers/volumeController.js
@@ -1,6 +1,7 @@
 const Volume = require("../models/volume");
 const Comic = require("../models/comic");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.volume_detail = asyncHandler(async (req, res, next) => {
   const volume = await Volume.findById(req.params.id).exec();
@@ -44,8 +45,6 @@ exports.volume_delete_post = asyncHandler(async (req, res, next) => {
   const volume = await Volume.findById(req.body.volumeid).exec();
   const associatedComic = await Comic.findOne({ volumes: volume._id }).exec();
 
-  console.log(req.body.volumeid);
-  console.log(associatedComic, volume);
   if (!volume) {
     const err = new Error("Volume not found");
     err.status = 404;
@@ -61,3 +60,65 @@ exports.volume_delete_post = asyncHandler(async (req, res, next) => {
   await Volume.findByIdAndDelete(req.body.volumeid);
   res.redirect(associatedComic.url);
 });
+
+exports.volume_update_get = asyncHandler(async (req, res, next) => {
+  const volume = await Volume.findById(req.params.id).exec();
+
+  res.render("volume_form", {
+    title: "Update volume",
+    volume: volume,
+    errors: [],
+  });
+});
+
+exports.volume_update_post = [
+  body("volume_number", "Volume number field must be filled")
+    .trim()
+    .notEmpty()
+    .escape(),
+
+  body("volume_title")
+    .trim()
+    .notEmpty()
+    .withMessage("Title field must be filled")
+    .isLength({ max: 100 })
+    .withMessage("This title is too long")
+    .escape(),
+
+  body("volume_description")
+    .trim()
+    .notEmpty()
+    .withMessage("Description field must be filled")
+    .isLength({ max: 200 })
+    .withMessage("This description is too long")
+    .escape(),
+
+  body("volume_release_date", "Invalid date")
+    .optional({ value: "falsy" })
+    .isISO8601()
+    .toDate()
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const volume = new Volume({
+      volume_number: req.body.volume_number || undefined,
+      title: req.body.volume_title || undefined,
+      description: req.body.volume_description || undefined,
+      release_date: req.body.volume_release_date || undefined,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("volume_form", {
+        title: "Update volume",
+        volume: volume,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    await Volume.findByIdAndUpdate(req.params.id, volume);
+    res.redirect(volume.url);
+  }),
+];
