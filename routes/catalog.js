3c73707efce6b591a85b81303dbf2c15fdb946db
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-24 13:44:36 +0100

    feat: Add author deletion routes
    - Added GET route for displaying the author deletion page.
    - Added POST route for handling the author deletion process.

--- routes/catalog.js
+++ routes/catalog.js
@@ -37,6 +37,8 @@ router.get("/comics", comic_controller.comic_list);
 // == AUTHOR controller == //
 router.get("/author/create", author_controller.author_create_get);
 router.post("/author/create", author_controller.author_create_post);
+router.get("/author/:id/delete", author_controller.author_delete_get);
+router.post("/author/:id/delete", author_controller.author_delete_post);
 router.get("/author/:id/update", author_controller.author_update_get);
 router.post("/author/:id/update", author_controller.author_update_post);
 router.get("/author/:id", author_controller.author_detail);
