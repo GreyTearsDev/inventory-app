1bc3d9abda2255191195022d35c3a4be2380ddfc
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-07-24 13:04:57 +0100

    feat: Add GET route for genre delete

--- routes/catalog.js
+++ routes/catalog.js
@@ -45,6 +45,7 @@ router.get("/authors", author_controller.author_list);
 // == GENRE controller == //
 router.get("/genre/create", genre_controller.genre_create_get);
 router.post("/genre/create", genre_controller.genre_create_post);
+router.get("/genre/:id/delete", genre_controller.genre_delete_get);
 router.get("/genre/:id/update", genre_controller.genre_update_get);
 router.post("/genre/:id/update", genre_controller.genre_update_post);
 router.get("/genre/:id", genre_controller.genre_detail);
