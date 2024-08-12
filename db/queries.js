5b5f93fd4f6d4255bf5a93277c865106b5b11340
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-08-12 18:35:58 +0100

    feat: Refactor query functions to exports and add queries for specific genre details and comics of a genre

--- db/queries.js
+++ db/queries.js
@@ -10,16 +10,16 @@ exports.selectAllGenres = async () => {
   }
 };
 
-async function selectAllPublishers() {
+exports.selectAllPublishers = async () => {
   try {
     const { rows } = await pool.query(`SELECT * FROM publishers ORDER BY name`);
     return rows;
   } catch (e) {
     console.log(e);
   }
-}
+};
 
-async function selectAllAuthors() {
+exports.selectAllAuthors = async () => {
   try {
     const { rows } = await pool.query(
       `SELECT * FROM authors ORDER BY first_name`,
@@ -28,18 +28,18 @@ async function selectAllAuthors() {
   } catch (e) {
     console.log(e);
   }
-}
+};
 
-async function selectAllComics() {
+exports.selectAllComics = async () => {
   try {
     const { rows } = await pool.query(`SELECT * FROM comics ORDER BY title`);
     return rows;
   } catch (e) {
     console.log(e);
   }
-}
+};
 
-async function selectAllVolumes() {
+exports.selectAllVolumes = async () => {
   try {
     const { rows } = await pool.query(
       `SELECT * FROM volumes ORDER BY volume_number`,
@@ -48,4 +48,36 @@ async function selectAllVolumes() {
   } catch (e) {
     console.log(e);
   }
-}
+};
+
+// SELECT query for getting data about a specific entry in a table
+exports.selectGenreDetails = async (genreId) => {
+  const text = "SELECT * FROM genres WHERE id = $1";
+  try {
+    const { rows } = await pool.query(text, [genreId]);
+    return rows;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// SELECT query for getting data about comics of a certain genre
+exports.selectComicsOfGenre = async (genreId) => {
+  const text = `SELECT 
+                  comics.id, 
+                  comics.title,
+                  comics.summary,
+                  comics.release_date,
+                  comics.author_id,
+                  comics.publisher_id,
+                  comics.url
+                FROM comics LEFT JOIN comics_genres AS cg 
+                            ON comics.id = cg.comic_id 
+                            AND cg.genre_id = $1`;
+  try {
+    const { rows } = await pool.query(text, [genreId]);
+    return rows;
+  } catch (e) {
+    console.log(e);
+  }
+};
