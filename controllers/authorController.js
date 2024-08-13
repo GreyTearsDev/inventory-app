20baa4918d80148a2f18d366bd9029c83ed5a259
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-08-13 11:03:21 +0100

    refactor: Remove unused imports for Author and Comic MongoDB schema definitions

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,3 @@
-const Author = require("../models/author");
-const Comic = require("../models/comic");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const db = require("../db/queries");
